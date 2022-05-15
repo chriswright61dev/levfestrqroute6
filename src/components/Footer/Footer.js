a93b15b9ab8e3531a0f7f26ffb2b883fc35273cc
Chris Wright <chriswrightdev@gmail.com>
2022-05-15 14:02:40 +0100

    added navigation to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,11 @@
+import "./Footer.css";
+import Navigation from "./Navigation/Navigation";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <Navigation />
+    </footer>
+  );
 }
 
 export default Footer;
---
+++ src/components/Footer/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <nav className="header-links">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/events">What's On</Link>
+        </li>
+        <li>
+          <Link to="/venues">Venues</Link>
+        </li>
+
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
