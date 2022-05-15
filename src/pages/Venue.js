4bd5680fd18bc41766c4fa624ab1672370d4636d
Chris Wright <chriswrightdev@gmail.com>
2022-05-15 14:32:53 +0100

    display venue list data

--- src/pages/Venue.js
+++ src/pages/Venue.js
@@ -1,5 +1,11 @@
-function Venue() {
-  return <div>Venue</div>;
+function Venue({ eventListData }) {
+  return (
+    <div className="content">
+      <div className="container">
+        <h1 className="page_h1">A Levenshulme Festival Venue</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Venue;
--- src/pages/Venues.js
+++ src/pages/Venues.js
@@ -1,5 +1,24 @@
+import useVenue from "../data/queryHooks/useVenue";
+import VenueList from "../modules/Venues/VenueList/VenueList";
+import Loader from "../components/Loader/Loader";
+
 function Venues() {
-  return <div>Venues</div>;
+  const allVenueData = useVenue();
+  // get all the venues
+
+  if (allVenueData.isLoading) {
+    return <Loader />;
+  } else {
+    const VenueData = allVenueData.data;
+
+    return (
+      <div className="content">
+        <h1 className="page_h1">Levenshulme Festival Venues</h1>
+
+        <VenueList venueListData={VenueData} />
+      </div>
+    );
+  }
 }
 
 export default Venues;
