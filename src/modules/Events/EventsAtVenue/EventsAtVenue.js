ad20a5189e1e1ff2d526aa34e5442b512e477f6a
Chris Wright <chriswrightdev@gmail.com>
2022-05-15 16:32:08 +0100

    added maps to venue

---
+++ src/modules/Events/EventsAtVenue/EventsAtVenue.js
@@ -0,0 +1,25 @@
+import "./EventsAtVenue.css";
+import EventCard from "../../../components/card/EventCard/EventCard";
+function EventsAtVenue({ venueEvents }) {
+  return (
+    <div className="eventsInVenue">
+      {venueEvents.map((event) => {
+        return (
+          <EventCard
+            key={event.id}
+            id={event.id}
+            link={"events"}
+            title={event.event_title}
+            name={event.event_name}
+            date={event.event_date_time}
+            introduction={event.event_introduction}
+            time={event.event_time_text}
+            image_1={event.event_image_1}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default EventsAtVenue;
--- src/pages/Venue.js
+++ src/pages/Venue.js
@@ -1,8 +1,11 @@
+import VenueDetails from "../modules/Venues/VenueDetails/VenueDetails";
+
 function Venue({ eventListData }) {
   return (
     <div className="content">
       <div className="container">
         <h1 className="page_h1">A Levenshulme Festival Venue</h1>
+        <VenueDetails eventListData={eventListData} />
       </div>
     </div>
   );
