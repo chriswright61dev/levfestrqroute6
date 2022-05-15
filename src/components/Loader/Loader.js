da78668bf9f8cd3ec11c92a1c70998107b730e1a
Chris Wright <chriswrightdev@gmail.com>
2022-05-15 13:37:14 +0100

    add loading data component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Loader.css";
+function Loader() {
+  return (
+    <div className="loader_container">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
