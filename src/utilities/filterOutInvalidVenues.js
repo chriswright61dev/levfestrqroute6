77e1e089f9a4df887d482493c5018dfc58804219
Chris Wright <chriswrightdev@gmail.com>
2022-05-17 23:21:56 +0100

    added venue filter function

--- src/utilities/filterOutInvalidVenues.js
+++ src/utilities/filterOutInvalidVenues.js
@@ -1,5 +1,16 @@
-function filterOutInvalidVenues() {
-  return <div>filterOutInvalidVenues</div>;
-}
+export default function filterOutInvalidVenues(allData, validList) {
+  // make list of venues with events in the year
 
-export default filterOutInvalidVenues;
+  // make sure the data is valid
+  if (allData && validList) {
+    // make it a set to remove duplicates then back to array
+    // because sets methods are poor
+    const validVenues = Array.from(
+      new Set(validList.map(({ venue }) => venue))
+    );
+    //filter out the venues with no events
+
+    return allData.filter((x) => validVenues.includes(x.venue_title));
+  }
+  return [];
+}
