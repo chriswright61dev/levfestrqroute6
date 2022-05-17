2a9d4b2f286caef3cf5c13992d10d7a42eb9256d
Chris Wright <chriswrightdev@gmail.com>
2022-05-17 22:48:04 +0100

    add old events page

---
+++ src/pages/OldEvents.js
@@ -0,0 +1,22 @@
+import EventsList from "../modules/Events/EventsList/EventsList";
+import useEventYear from "../data/queryHooks/useEventYear";
+import Loader from "../components/Loader/Loader";
+function OldEvents() {
+  const thisYear = new Date().getFullYear();
+  // add some state for the year and a dropdown to change it
+  const allEventData = useEventYear(thisYear);
+  // get data by year
+  if (allEventData.isLoading) {
+    return <Loader />;
+  } else {
+    return (
+      <div className="content">
+        <h1 className="page_h1">Levenshulme Festival Events {thisYear}</h1>
+
+        <EventsList eventListData={allEventData.data} />
+      </div>
+    );
+  }
+}
+
+export default OldEvents;
